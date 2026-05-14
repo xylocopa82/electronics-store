e958584f32ff57d06b780b0e612794ea23018bcc
xylocopa82 <abdtho@gmail.com>
2026-05-14 11:47:28 -0700

    Added PostgreSQL database configuration

--- eg_store_project/settings.py
+++ eg_store_project/settings.py
@@ -122,13 +122,14 @@ WSGI_APPLICATION = 'eg_store_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
