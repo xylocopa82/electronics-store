ecd943af46c914ff2cc23ac0557ac36433703d6d
xylocopa82 <abdtho@gmail.com>
2026-05-15 10:41:11 -0700

    Removed cloudinary_storage from INSTALLED_APPS

--- eg_store_project/settings.py
+++ eg_store_project/settings.py
@@ -79,7 +79,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'accounts',
